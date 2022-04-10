5519ccd0ff4ded521855b01a84f233a01999b4f8
Rajiv Sharma <rajshar.email@gmail.com>
2022-04-10 12:52:22 +0100

    Minor fixes

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -2,14 +2,27 @@ use anyhow;
 use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
-    let (a,b, c) = include_str!("input.txt")
+    // Part 1
+    let (a,b) = include_str!("input.txt")
         .lines()
         .map(str::parse::<i64>)
         .collect::<Result<Vec<_>,_>>()?
         .into_iter()
         .tuple_combinations()
-        .find(|(a,b, c)| a + b + c == 2020)
-        .expect("No triplet in the input sums to 2020");
+        .find(|(a,b)| a + b == 2020)
+        .expect("No pair in the input sums to 2020");
+    dbg!(a + b);
+    dbg!(a * b);
+
+    // Part 2
+    let (a,b, c) = include_str!("input.txt")
+    .lines()
+    .map(str::parse::<i64>)
+    .collect::<Result<Vec<_>,_>>()?
+    .into_iter()
+    .tuple_combinations()
+    .find(|(a,b, c)| a + b + c == 2020)
+    .expect("No triplet in the input sums to 2020");
     dbg!(a + b + c);
     dbg!(a * b * c);
     Ok(())
