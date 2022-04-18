e12bb96a371c2caf9020ca16e5b1068d10684538
Rajiv Sharma <rajshar.email@gmail.com>
2022-04-18 22:37:02 +0100

    AdventOfCode 2020: Day 8 Part II

--- 2020/day8/src/main.rs
+++ 2020/day8/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use itertools::Itertools;
 
 #[derive(Debug, Clone, Copy)]
 enum InstructionKind {
@@ -22,21 +23,25 @@ struct State {
 }
 
 impl State {
-    fn next(self, program: &Program) -> Self {
-        let ins = program[self.pc];
-        match ins.kind {
-            InstructionKind::Nop => Self {
-                pc: self.pc + 1,
-                ..self
-            },
-            InstructionKind::Acc => Self {
-                pc: self.pc + 1,
-                acc: self.acc + ins.operand
-            },
-            InstructionKind::Jmp => Self {
-                pc: (self.pc as isize + ins.operand).try_into().unwrap(),
-                ..self
-            }
+    fn next(self, program: &Program) -> Option<Self> {
+        if !(0..program.len()).contains(&self.pc) {
+            None
+        } else {
+            let ins = program[self.pc];
+            Some(match ins.kind {
+                InstructionKind::Nop => Self {
+                    pc: self.pc + 1,
+                    ..self
+                },
+                InstructionKind::Acc => Self {
+                    pc: self.pc + 1,
+                    acc: self.acc + ins.operand
+                },
+                InstructionKind::Jmp => Self {
+                    pc: (self.pc as isize + ins.operand).try_into().unwrap(),
+                    ..self
+                }
+            })            
         }
     }
 }
@@ -65,13 +70,65 @@ fn parse_program(input: &str) -> Program {
         .collect()
 }
 
+fn eval(program: &Program) -> Option<isize> {
+    itertools::iterate(Some(State::default()), |state| {
+        state.and_then(|state| state.next(program))
+    })
+    .while_some()
+    .last()
+    .map(|s| s.acc)
+}
+
+fn flip_kind(kind: &mut InstructionKind) {
+    *kind = match *kind {
+        InstructionKind::Jmp => InstructionKind::Nop,
+        InstructionKind::Nop => InstructionKind::Jmp,
+        x => x,
+    };
+}
+
+fn find_variant(program: &Program) {
+    let mut variants: Vec<_> = program
+        .iter()
+        .enumerate()
+        .filter_map(|(index, ins)| match ins.kind {
+            InstructionKind::Jmp | InstructionKind::Nop => Some(index),
+            _ => None,
+        })
+        .map(|i| {
+            let mut variant = program.clone();
+            flip_kind(&mut variant[i].kind);
+            (i, variant)
+        })
+        .map(|(index, variant)| {
+            itertools::iterate(Some(State::default()), move |state| {
+                state
+                    .unwrap_or_else(|| panic!("variant {} terminated!", index))
+                    .next(&variant)
+            })
+        })
+        .collect();
+
+    loop {
+        for v in &mut variants {
+            v.next();
+        }
+    }
+}
+
 fn main() {
+    // Part I
     let program = parse_program(include_str!("input.txt"));
-    let mut iter = itertools::iterate(State::default(), |s| s.next(&program));
+    let mut iter = itertools::iterate(State::default(), |s| s.next(&program).unwrap());
     let mut set: HashSet<usize> = Default::default();
     let answer = iter.find(|state| !set.insert(state.pc)).unwrap();
     println!(
         "Before executing {} a second time, the accumulator was {}",
         answer.pc, answer.acc
     );
+
+    // Part II
+    let mut program = parse_program(include_str!("input.txt"));
+    flip_kind(&mut program[381].kind);
+    dbg!(eval(&program));
 }
