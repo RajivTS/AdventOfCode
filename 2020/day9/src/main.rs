91b789688897499c543246e87556b9ac8171b844
Rajiv Sharma <rajshar.email@gmail.com>
2022-04-23 19:02:42 +0100

    AdventOfCode 2020: Day 9 Part II

--- 2020/day9/src/main.rs
+++ 2020/day9/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 
 fn main() {
-    // Part 1
+    // Part I
     let numbers = include_str!("input.txt")
         .lines()
         .map(|x| x.parse().unwrap())
@@ -20,4 +20,23 @@ fn main() {
         }
     });
     println!("The first weakness in XMAS data: {:?}", &answer);
+
+    // Part II
+    let answer = answer.unwrap();
+    let weakness = (2..numbers.len())
+        .into_iter()
+        .map(|n| {
+            numbers
+                .windows(n)
+                .enumerate()
+                .map(move |(i, window)| (i, n, window.iter().sum::<usize>()))
+        })
+        .flatten()
+        .find(|&(_, _, sum)| sum == answer);
+    let (start, size, _) = weakness.unwrap();
+    let data_set = &numbers[start..(start + size)];
+    println!(
+        "The final weakness in the XMAS data: {:?}",
+        (data_set.iter().min().unwrap() + data_set.iter().max().unwrap())
+    );
 }
